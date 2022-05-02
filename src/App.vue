35a8bd002da5fa2d8b1b930b6222790e94a9515d
siz0001 <42665078+siz0001@users.noreply.github.com>
2022-05-03 02:36:09 +0900

    check

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+     <v-app-bar app dark>
+      <v-app-bar-title> 아이디어 앱 </v-app-bar-title> <v-spacer></v-spacer>
+      
+      <v-btn v-if="user" text @click="logout()">로그아웃</v-btn>
+      <v-btn v-else text to="/login">로그인</v-btn>
+    </v-app-bar>
+
+   
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    user: null,
+  }),
+  computed:{
+    // 자동으로 바뀌어야 하는 변수들 
+  },
+  methods:{
+    // 함수들
+  },
+  created(){
+    // 페이지에 진입했을때 자동으로 실행될 함수들(페이지 구현전 실행)
+  },
+  mounted(){
+    // 페이지에 진입했을때 페이지가 구현된후 자동으로 실행될 함수들 
+  },
+  watch:{
+    // 특정 값이 변경될경우 실행되어야 할 함수들
+  }
+};
+</script>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="">
+  
 </style>
